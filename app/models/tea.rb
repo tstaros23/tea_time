391f2de5e0739e49ba517a41e3eb11d1712ff430
Ted Staros <tstaros23@gmail.com>
2022-03-01 23:10:31 -0700

    Finish tea model testing

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,4 @@
 class Tea < ApplicationRecord
+  belongs_to :subscription
+  validates_presence_of :title, :description, :temperature, :brew_time
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:subscription) }
+  end
 end
